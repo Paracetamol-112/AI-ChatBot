1837ac2ecbf902eba5e190c90ac0b7455666da68
Paracetamol-112 <108113910+Paracetamol-112@users.noreply.github.com>
2025-10-10 14:27:08 +0800

    Connect the frontend and backend

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+
+      // The above mean that when you send the request to /api/hello
+      // it will automatically forward to http://localhost:3000/api/hello
+    }
+  }
 })
