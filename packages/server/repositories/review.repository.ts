b030156de680d97bc3cfeb04b9c75a575677d2f8
Paracetamol-112 <108113910+Paracetamol-112@users.noreply.github.com>
2025-10-21 21:54:30 +0800

    Refactoring separation of concerns

---
+++ packages/server/repositories/review.repository.ts
@@ -0,0 +1,15 @@
+import { PrismaClient, type Review } from "../generated/prisma";
+
+export const reviewRepository = {
+   async getReviews(productId: number): Promise<Review[]> {
+      // SELECT * FROM reviews WHERE productId = @productId ORDER BY createdAt DESC
+      // will be generated
+      // by prisma when we run the following code
+      const prisma = new PrismaClient();
+
+      return prisma.review.findMany({
+         where: { productId },
+         orderBy: { createdAt: "desc" },
+      });
+   },
+};
