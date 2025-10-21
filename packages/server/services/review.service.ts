2b89933c1d08ff17c9fc4d3ca68714f33f21a868
Paracetamol-112 <108113910+Paracetamol-112@users.noreply.github.com>
2025-10-21 23:11:04 +0800

    Refactoring extracting the prompt

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,6 +1,7 @@
 import { type Review } from "../generated/prisma";
 import { reviewRepository } from "../repositories/review.repository";
 import { llmClient } from "../llm/client";
+import template from "../prompts/summarize-reviews.txt";
 
 export const reviewService = {
    async getReviews(productId: number): Promise<Review[]> {
@@ -12,12 +13,8 @@ export const reviewService = {
       const reviews = await reviewRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join("\n\n");
       // Send the reviews to a LLM
-      const prompt = `
-         Summarize the following customer reviews into a short paragraph
-         highlighting key themes, both positive and negative: 
+      const prompt = template.replace("{{reviews}}", joinedReviews);
 
-         ${joinedReviews}
-      `;
       const response = await llmClient.generatedText({
          model: "gpt-4.1",
          prompt: prompt,
