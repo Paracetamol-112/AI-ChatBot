3cf64d6a213fbe811e24f26c791ccaa5540c266b
Paracetamol-112 <108113910+Paracetamol-112@users.noreply.github.com>
2025-10-21 21:31:04 +0800

    Creating the api to fetch reviews

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { chatController } from "./controllers/chat.controller";
+import { PrismaClient } from "./generated/prisma";
 
 const router = express.Router();
 
@@ -9,9 +10,29 @@ router.get("/", (req: Request, res: Response) => {
 });
 
 router.get("/api/hello", (req: Request, res: Response) => {
-   res.json({ message: "Hello World!" });
+   res.json({ message: "Hello World!!!!!" });
 });
 
 router.post("/api/chat", chatController.sendMessage);
+
+router.get("/api/products/:id/reviews", async (req: Request, res: Response) => {
+   const prisma = new PrismaClient();
+   const productId = Number(req.params.id);
+
+   if (isNaN(productId)) {
+      res.status(400).json({ error: "Invalid product ID." });
+      return;
+   }
+
+   // SELECT * FROM reviews WHERE productId = @productId ORDER BY createdAt DESC
+   // will be generated
+   // by prisma when we run the following code
+   const reviews = await prisma.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: "desc" },
+   });
+
+   res.json(reviews);
+});
 
 export default router;
